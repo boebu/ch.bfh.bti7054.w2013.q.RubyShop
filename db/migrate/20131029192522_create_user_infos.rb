ce08b41eaada4c581a4a09bd60b04595d3820baa
Marco Füllemann <mfuellemann@gmail.com>
2013-10-29 20:55:00 +0100

    userInfo added

---
+++ db/migrate/20131029192522_create_user_infos.rb
@@ -0,0 +1,14 @@
+class CreateUserInfos < ActiveRecord::Migration
+  def change
+    create_table :user_infos do |t|
+      t.string :firstName
+      t.string :lastName
+      t.string :street
+      t.string :zip
+      t.string :place
+      t.string :mail
+      t.string :phone
+      t.timestamps
+    end
+  end
+end
