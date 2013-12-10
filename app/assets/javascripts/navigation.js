9587522417ceaae8adbadcfcf44439a215b3b3b3
Marco Füllemann <mfuellemann@gmail.com>
2013-12-10 17:18:47 +0100

    navigation updated

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -3,5 +3,14 @@
  */
 
 $("li").click(function(e) {
-    $(this).siblings("ul").toggle();
+    e.stopPropagation();
+    var obj = $(this);
+    var parentLi = obj.parent('ul').parent('li');
+    var ids = [obj.attr('rel')];
+    obj.children("ul").toggle();
+    while (parentLi.length) {
+        ids.push(parentLi.attr('rel'));
+        parentLi = parentLi.parent('ul').parent('li');
+    }
+    console.log("Ids to fetch: " + ids.join(', '));
 });
