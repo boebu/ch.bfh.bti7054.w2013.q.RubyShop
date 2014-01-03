4c46cea486fbd5377537e105bfd1459be978175f
flec <mfuellemann@gmail.com>
2014-01-03 17:10:53 +0100

    styled

--- app/helpers/shop_helper.rb
+++ app/helpers/shop_helper.rb
@@ -1,8 +1,8 @@
 module ShopHelper
 
 
-  def link_add_to_cart(name,item)
-   link_to_function(name, "addToCart(\"#{item}\")", class: 'button')
+  def link_add_to_cart(item)
+   link_to_function("", "addToCart(\"#{item}\")", class: 'button foundicon-cart')
   end
 
 end
