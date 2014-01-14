abd9dc8fe0bd429ae6b2039811d8a37dc5915f0a
flec <mfuellemann@gmail.com>
2014-01-14 14:47:34 +0100

    validation

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -1,3 +1,9 @@
 class UserInfo < ActiveRecord::Base
   belongs_to :user
+  validates :firstName,  presence: true
+  validates :lastName,  presence: true
+  validates :phone, numericality: { only_integer: true }
+  validates :street,  presence: true
+  validates :zip, numericality: { only_integer: true }
+  validates :place,  presence: true
 end
